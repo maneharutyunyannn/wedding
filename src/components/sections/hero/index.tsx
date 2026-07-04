f36465b027f9718bcac3cb7214863e6695f50339
maneharutyunyannn <manharutyunyan98@gmail.com>
2026-07-04 23:30:50 +0400

    change

--- src/components/sections/hero/index.tsx
+++ src/components/sections/hero/index.tsx
@@ -5,7 +5,8 @@ import { cn } from "@/lib/utils";
 
 export function HeroSection() {
     return (
-        <section className="relative flex min-h-screen snap-start items-end justify-center pb-20 text-center text-white">
+        <section
+            className="relative flex min-h-[100dvh] snap-start items-end justify-center pb-20 text-center text-white">
             <h2 className={cn(dzeragir.className, "text-5xl")}>
                 Հովհաննես և Լիանա
             </h2>
